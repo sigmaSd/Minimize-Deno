1a27ab847810210b1c3a5fae5c04e3650a5460a5
Nbiba Bedis <bedisnbiba@gmail.com>
2023-12-10 10:41:53 +0100

    update deps

--- tests/mod.test.ts
+++ tests/mod.test.ts
@@ -1,7 +1,7 @@
 import {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.206.0/assert/mod.ts";
+} from "https://deno.land/std@0.208.0/assert/mod.ts";
 import { Permission } from "../main.ts";
 
 const D = new TextDecoder();
